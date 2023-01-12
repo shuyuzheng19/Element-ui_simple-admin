0a2f1a2d0dcf1fbe8bacde47e6d44eb151f9d7e5
shuyuzheng19 <105098546+shuyuzheng19@users.noreply.github.com>
2023-01-12 21:21:38 +0800

    Update Layout.vue

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -21,42 +21,9 @@
             LeftNav,
             TopHeader,
             Main
-        },
-        data(){
-            return{
-                isCollapse: true,
-                clientHeight: 0,
-                iframeUrl: 'http://www.taobao.com',
-            }
-        },
-        methods:{
-            handleOpen(key, keyPath) {
-                console.log(key, keyPath);
-            },
-            handleClose(key, keyPath) {
-                console.log(key, keyPath);
-            },
-            reloadUrl(){
-                document.getElementById("iframe").src=this.iframeUrl;
-            }
-        },
-        mounted() {
-            let h = document.documentElement.clientHeight || document.body.clientHeight;
-            this.clientHeight=h;
         }
     }
 </script>
 
 <style scoped>
-
-
-    #content {
-        margin: 0!important;
-        padding: 0!important;
-        overflow: hidden;
-    }
-
-    .top {
-        margin-top: 15px!important;
-    }
 </style>
